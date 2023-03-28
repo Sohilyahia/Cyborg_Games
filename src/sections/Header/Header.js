d361b090afe7ed923622361b720c5a3041b4a155
Sohilyahya <sohilyahya11@gmail.com>
2023-03-28 22:01:46 +0200

    edit

--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -9,53 +9,7 @@ import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
   return (
-    // <div className="navbar navbar-expand-md navbar-dark cyborg-navbar">
-    //   <div className="container">
-    //     <Link to="/" className="navbar-brand">
-    //       <img src={logo} alt="Cyborg Logo" />
-    //     </Link>
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-bs-toggle="collapse"
-    //       data-bs-target="#mainmenu"
-    //     >
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
 
-    //     <div className="collapse navbar-collapse" id="mainmenu">
-    //       <ul className="navbar-nav ms-auto">
-    //         <NavItems>
-    //           <NavLink to="/" className="nav-link">
-    //             Home
-    //           </NavLink>
-    //         </NavItems>
-    //         <NavItems>
-    //           <NavLink to="/browse" className="nav-link">
-    //             Browse
-    //           </NavLink>
-    //         </NavItems>
-
-    //         <NavItems>
-    //           <NavLink to="/details" className="nav-link">
-    //             Details
-    //           </NavLink>
-    //         </NavItems>
-
-    //         <NavItems>
-    //           <NavLink to="/streams" className="nav-link">
-    //             Stream
-    //           </NavLink>
-    //         </NavItems>
-    //         <NavItems>
-    //           <NavLink to="/profile" className="nav-link">
-    //             Profile
-    //           </NavLink>
-    //         </NavItems>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </div>
 
     <Navbar
       collapseOnSelect
